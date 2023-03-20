e8e8c99d85042b9456e1d181d2cc71fc3006902b
Flávio Machado Galante <109998820+flaviogalante@users.noreply.github.com>
2023-03-20 16:41:27 -0300

    Update Pessoa.cs

--- T16CodBackEnd/Classes/Pessoa.cs
+++ T16CodBackEnd/Classes/Pessoa.cs
@@ -4,19 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using T16CodBackEnd.Interfaces;
 
-
 namespace T16CodBackEnd.Classes
 {
     public abstract class Pessoa : IPessoa
     {
         public string? nome { get; set; }
-        public string? endereco { get; set; }
+        public Endereco? endereco { get; set; }
         public float rendimento { get; set; }
 
 
-        public float CalcularImposto(float rendimento)
-        {
-            throw new NotImplementedException();
-        }
+        public abstract float CalcularImposto(float rendimento);
+        
     }
 }
